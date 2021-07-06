72f07c2ab70453caec1fd44a249af5c43a92d8ac
Viktoriia Chumak <vikaff17@gmail.com>
2021-07-06 14:42:12 +0300

    add js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
 import './sass/main.scss';
-
-const anchors = document.querySelectorAll('a.scroll-to')
-
-for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-    
-    const blockID = anchor.getAttribute('href')
-    
-    document.querySelector(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
-}
